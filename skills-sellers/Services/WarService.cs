fc74c6d1961e7383505512609fdf739e9b12a29b
Anthony Paboeuf <62709270+Delsus78@users.noreply.github.com>
2024-04-04 15:41:50 +0200

    Update WarService.cs

--- skills-sellers/Services/WarService.cs
+++ skills-sellers/Services/WarService.cs
@@ -536,7 +536,7 @@ public class WarService : IWarService
                 
                 // deleting registre and all registres with a name containing the same name (ex: "Tue" will delete "Tueur", "Tueur de la mort", etc)
                 
-                var registresToDelete = _context.Registres.Where(r => r.Name.Contains(registreTarget.Name)).ToList();
+                var registresToDelete = _context.Registres.Where(r => r.Name.ToUpperCase().Contains(registreTarget.Name.ToUpperCase())).ToList();
                 
                 foreach (var registre in registresToDelete)
                 {
